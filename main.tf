e380710f21c82f2aaa530fd58801311a0c5b1280
Harihar <harihar.mishra.ext@nokia.com>
2025-10-06 14:17:40 +0530

    comit06102025

--- main.tf
+++ main.tf
@@ -8,7 +8,30 @@ terraform {
 
 provider "google" {
 
-  project = "Project ID"
-  region  = "Region"
-  zone    = "Zone"
+  project = "qwiklabs-gcp-01-c1f378517fc8"
+  region  = "us-west1"
+  zone    = "us-west1-a"
 }
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "e2-medium"
+  tags         = ["web" , "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+
+  }
+  allow_stopping_for_update = true
+}
+
+
+
+
+
